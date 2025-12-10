ad72a0351f3f979f500e2280010cb95efbedeaaa
opsdlamini <dowethu413@gmail.com>
2025-12-10 22:46:02 +0200

    feat(auth): add hashing and verification for passwords, and creation of access and refresh tokens

--- backend/app/core/security.py
+++ backend/app/core/security.py
@@ -1,2 +1,28 @@
 # Security utilities
 
+from passlib.context import CryptContext
+from jose import jwt
+from datetime import datetime, timedelta
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+SECRET_KEY = "d97c0d1633a6da68d9f7393c087e9d8b"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+REFRESH_TOKEN_EXPIRE_DAYS = 30 # TODO: To be discussed
+
+def hash_password(password: str):
+    return pwd_context.hash(password)
+
+def verify_password(password: str, hashed: str):
+    return pwd_context.verify(password, hashed)
+
+def create_access_token(data: dict):
+    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    data.update({"exp": expire})
+    return jwt.encode(data, SECRET_KEY, algorithm=ALGORITHM)
+
+def create_refresh_token(data: dict):
+    expire = datetime.utcnow() + timedelta(days=REFRESH_TOKEN_EXPIRE_DAYS)
+    data.update({"exp": expire, "type": "refresh"})
+    return jwt.encode(data, SECRET_KEY, algorithm=ALGORITHM)
