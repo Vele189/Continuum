5a7ca267f705db4cbd54183e127360fa2f203fcb
Letha456 <u23535212@tuks.co.za>
2025-12-22 23:42:55 +0200

    feat(db): Added Updated Database

--- backend/app/database.py
+++ backend/app/database.py
@@ -199,6 +199,9 @@ class Task(Base):
         nullable=True,
         index=True
     )
+    
+    
+    due_date = Column(DateTime(timezone=True), nullable=True)
 
     created_at = Column(DateTime(timezone=True), server_default=func.now())
     updated_at = Column(
