086923ffa3a1e7aec8170341d23eb2fb3d6cdb94
Urangani <www.uranganidc@gmail.com>
2025-12-30 00:57:22 +0200

    feat(auth):added forgot & reset password page

--- continuum-frontend/src/pages/Auth/ForgotPassword.tsx
+++ continuum-frontend/src/pages/Auth/ForgotPassword.tsx
@@ -1,35 +1,39 @@
-import { FaArrowLeft } from "react-icons/fa"; 
+import { FaArrowLeft } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
-
-const ForgotPassword = () =>{
+const ForgotPassword = () => {
   return (
-    
-   
-    <div className="bg-gradient-to-b from-[#B2E6F7] to-[#FFFFFF] min-h-screen flex items-center justify-center relative overflow-hidden ">
+    <div className="bg-linear-to-b from-[#B2E6F7] to-[#FFFFFF] min-h-screen flex items-center justify-center relative overflow-hidden ">
+      <div className="bg-white w-86.25 h-75.75  flex flex-col justify-center items-center px-6 pt-2 pb-9 border-2 border-gray-100 rounded-2xl shadow-lg">
+        <div className="bg-[#F9F9F9]  flex items-center gap-4 mb-6 w-full text-[22px] font-medium text-[#595959]">
+          <Link to="/login">
+            <FaArrowLeft className="text-[20px] font-light" />
+          </Link>
+          <h2 className="mb-0 ml-12">Forgot Password</h2>
+        </div>
 
-      <div className="bg-white w-86.25 h-75.75  flex flex-col justify-center items-center px-[24px] pt-[24px] pb-[36px] rounded-2xl shadow-lg">
-
-      <div className="bg-[#F9F9F9]  flex items-center gap-4 mb-6 w-full text-2xl font-medium text-[#595959]"> 
-          <FaArrowLeft className="text-xl" /> 
-          <h2 className="mb-0 mx-6">Forgot Password</h2>
-      </div>
-
-
-        <p className="text-gray-400 mb-6">We will email a reset link to your email address you used.</p>
+        <p className="text-gray-400 mb-6 text-[16px]">
+          We will email a reset link to your email address you used.
+        </p>
 
         <form className="w-full max-w-sm">
           <div className="mb-4">
-            <label htmlFor="email" className="block text-gray-700 mb-2">Email</label>
+            <label
+              htmlFor="email"
+              className="block text-gray-900 mb-2 text-[16px]"
+            >
+              Email
+            </label>
             <input
               type="email"
               id="email"
-              className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring focus:ring-blue-500"
+              className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring focus:ring-blue-500 text-[16px]"
               placeholder="you@example.com"
             />
           </div>
           <button
             type="submit"
-            className="w-full bg-blue-500 text-white py-2 px-4 rounded-lg hover:bg-blue-600 transition duration-300"
+            className="w-full bg-[#2299fa] text-white text-[16px] font-bold  py-2 px-4 rounded-lg"
           >
             Send reset link
           </button>
@@ -37,12 +41,6 @@ const ForgotPassword = () =>{
       </div>
     </div>
   );
-
-}
-
-
-
-
+};
 
 export default ForgotPassword;
-
