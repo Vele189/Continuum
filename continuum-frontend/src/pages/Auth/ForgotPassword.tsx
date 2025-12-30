51e27e0b02690e33ce942986ce89e910b4a724ac
Urangani <www.uranganidc@gmail.com>
2025-12-30 10:56:07 +0200

    feat(auth): pages optimization

--- continuum-frontend/src/pages/Auth/ForgotPassword.tsx
+++ continuum-frontend/src/pages/Auth/ForgotPassword.tsx
@@ -4,20 +4,22 @@ import { Link } from "react-router-dom";
 const ForgotPassword = () => {
   return (
     <div className="bg-linear-to-b from-[#B2E6F7] to-[#FFFFFF] min-h-screen flex items-center justify-center relative overflow-hidden ">
-      <div className="bg-white w-86.25 h-75.75  flex flex-col justify-center items-center px-6 pt-2 pb-9 border-2 border-gray-100 rounded-2xl shadow-lg">
-        <div className="bg-[#F9F9F9]  flex items-center gap-4 mb-6 w-full text-[22px] font-medium text-[#595959]">
+      <div className="relative bg-white w-86.25 h-75.75  flex flex-col justify-center items-center pt-2 border-2 border-gray-100 rounded-2xl shadow-lg">
+        <div className="absolute top-0 left-0 h-[54px] rounded-t-lg px-6 bg-[#F9F9F9] gap-4 flex items-center mb-6 w-full text-[22px] font-medium text-[#595959]">
           <Link to="/login">
             <FaArrowLeft className="text-[20px] font-light" />
           </Link>
           <h2 className="mb-0 ml-12">Forgot Password</h2>
         </div>
 
-        <p className="text-gray-400 mb-6 text-[16px]">
+    <div className="px-6 h-[249px]">
+
+        <p className="text-gray-400 mt-4 mb-6 text-[16px]">
           We will email a reset link to your email address you used.
         </p>
 
-        <form className="w-full max-w-sm">
-          <div className="mb-4">
+        <form className="w-full">
+          <div>
             <label
               htmlFor="email"
               className="block text-gray-900 mb-2 text-[16px]"
@@ -27,7 +29,7 @@ const ForgotPassword = () => {
             <input
               type="email"
               id="email"
-              className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring focus:ring-blue-500 text-[16px]"
+              className="w-full px-4 py-2 mb-6 border border-gray-300 rounded-lg focus:outline-none focus:ring focus:ring-blue-500 text-[16px]"
               placeholder="you@example.com"
             />
           </div>
@@ -38,6 +40,8 @@ const ForgotPassword = () => {
             Send reset link
           </button>
         </form>
+    </div>
+
       </div>
     </div>
   );
--- continuum-frontend/src/pages/Auth/ResetPassword.tsx
+++ continuum-frontend/src/pages/Auth/ResetPassword.tsx
@@ -5,18 +5,20 @@ const ResetPassword = () => {
   return (
     <div className="bg-linear-to-b from-[#B2E6F7] to-[#FFFFFF] min-h-screen flex items-center justify-center relative overflow-hidden ">
 
-      <div className="bg-white flex flex-col justify-center w-[450px] items-center px-6 pt-6 pb-9 rounded-2xl border-2 border-gray-100 shadow-lg">
+      <div className="relative bg-white flex flex-col justify-center w-[400px] items-center px-6 pb-9 rounded-2xl border-2 border-gray-100 shadow-lg">
 
-        <div className="bg-[#F9F9F9]  flex items-center gap-4 mb-8 w-full text-[22px] font-medium text-[#595959]">
+        <div className="absolute top-0 left-0 h-[54px] bg-[#F9F9F9]  flex items-center px-6 mb-8 w-full text-[22px] rounded-t-2xl font-medium text-[#595959]">
           <Link to="/login">
             <FaArrowLeft className="text-[20px] font-light" />
           </Link>
           <h2 className="mb-0 ml-18 font-medium text-[20px]">Reset Password</h2>
         </div>
 
-        <form className="w-full max-w-sm">
+        <div className="w-full mt-16">
+
+        <form className="">
           <div className="mb-4">
-            <label htmlFor="new-password" className="block text-gray-700 mb-2 text-[20px]"><strong>New password</strong></label>
+            <label htmlFor="new-password" className="block text-gray-700 mb-2 text-[19px]"><strong>New password</strong></label>
             <input
               type="password"
               id="new-password"
@@ -25,7 +27,7 @@ const ResetPassword = () => {
             />
           </div>
           <div className="mb-6">
-            <label htmlFor="confirm-password" className="block text-gray-700 mb-2 text-[20px]"><strong>Confirm new password</strong></label>
+            <label htmlFor="confirm-password" className="block text-gray-700 mb-2 text-[19px]"><strong>Confirm new password</strong></label>
             <input
               type="password"
               id="confirm-password"
@@ -40,6 +42,7 @@ const ResetPassword = () => {
             Reset Password
           </button>
         </form>
+        </div>
       </div>
     </div>
   );
