1ab80f5e1736e871af61b4d70e96ea4acab99b97
Urangani <www.uranganidc@gmail.com>
2026-01-03 15:50:23 +0200

    feat(auth): fixed  Forgot and Reset Password pages

--- continuum-frontend/src/pages/Auth/ForgotPassword.tsx
+++ continuum-frontend/src/pages/Auth/ForgotPassword.tsx
@@ -1,41 +1,49 @@
-import { FaArrowLeft } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import backArrowIcon from "../../assets/back-arrow.png"
 
 const ForgotPassword = () => {
   return (
     <div className="bg-linear-to-b from-[#B2E6F7] to-[#FFFFFF] min-h-screen flex items-center justify-center relative overflow-hidden ">
-      <div className="relative bg-white w-86.25 h-75.75  flex flex-col justify-center items-center pt-2 border-2 border-gray-100 rounded-2xl shadow-lg">
-        <div className="absolute top-0 left-0 h-[54px] rounded-t-lg px-6 bg-[#F9F9F9] gap-4 flex items-center mb-6 w-full text-[22px] font-medium text-[#595959]">
-          <Link to="/login">
-            <FaArrowLeft className="text-[20px] font-light" />
-          </Link>
-          <h2 className="mb-0 ml-12">Forgot Password</h2>
-        </div>
 
-    <div className="px-6 h-[249px]">
+      <div className="bg-white w-[345px] h-[303px] border-2 border-gray-100 rounded-2xl shadow-lg flex flex-col justify-center items-center">
 
-        <p className="text-gray-400 mt-4 mb-6 text-[16px]">
-          We will email a reset link to your email address you used.
-        </p>
+<div className="relative h-[54px] rounded-t-lg bg-[#F5F5F5] flex items-center justify-between w-full font-medium text-[#595959]">
+  <Link to="/login">
+    <img src={backArrowIcon} alt="Back Arrow" className="absolute top-3 left-4 h-4" />
+  </Link>
+  <h2 className="text-[16px] mx-auto">Forgot Password</h2>
+</div>
 
-        <form className="w-full">
-          <div>
+
+
+    <div className="w-[345px] h-[249px] flex flex-col justify-center items-center gap-6">
+
+<p
+  className="w-[297px] h-[38px] text-gray-400 font-medium text-[14px] leading-[100%] tracking-[0]"
+>
+  We will email a reset link to your email address you used.
+</p>
+
+
+        <form className="w-full flex flex-col items-center gap-[24px]">
+          <div className="w-[297px] h-[63px] flex flex-col gap-1 font-medium">
             <label
               htmlFor="email"
-              className="block text-gray-900 mb-2 text-[16px]"
+              className="block text-gray-900 text-[14px]"
             >
               Email
             </label>
             <input
               type="email"
               id="email"
-              className="w-full px-4 py-2 mb-6 border border-gray-300 rounded-lg focus:outline-none focus:ring focus:ring-blue-500 text-[16px]"
-              placeholder="you@example.com"
+              className="w-full h-[40px] px-4 py-2 border text-[14px] border-gray-300 rounded-lg focus:outline-none focus:ring focus:ring-blue-500"
+              placeholder="amushiringani@gmail.com"
             />
           </div>
+
           <button
             type="submit"
-            className="w-full bg-[#2299fa] text-white text-[16px] font-bold  py-2 px-4 rounded-lg"
+            className="w-[297px] h-[40px] bg-[#2299fa] text-white text-[14px] font-bold  py-2 px-4 rounded-lg"
           >
             Send reset link
           </button>
--- continuum-frontend/src/pages/Auth/ResetPassword.tsx
+++ continuum-frontend/src/pages/Auth/ResetPassword.tsx
@@ -1,48 +1,60 @@
-import { FaArrowLeft } from "react-icons/fa";
+import backArrowIcon from "../../assets/back-arrow.png"
 import { Link } from "react-router-dom";
 
 const ResetPassword = () => {
   return (
     <div className="bg-linear-to-b from-[#B2E6F7] to-[#FFFFFF] min-h-screen flex items-center justify-center relative overflow-hidden ">
 
-      <div className="relative bg-white flex flex-col justify-center w-[400px] items-center px-6 pb-9 rounded-2xl border-2 border-gray-100 shadow-lg">
-
-        <div className="absolute top-0 left-0 h-[54px] bg-[#F9F9F9]  flex items-center px-6 mb-8 w-full text-[22px] rounded-t-2xl font-medium text-[#595959]">
+      <div className="bg-white flex flex-col w-[345px] h-[320px] items-center rounded-2xl border-2 border-gray-100 shadow-lg">
+        <div className="relative h-[54px] rounded-t-lg bg-[#F5F5F5] flex items-center justify-between w-full font-medium text-[#595959]">
           <Link to="/login">
-            <FaArrowLeft className="text-[20px] font-light" />
+            <img src={backArrowIcon} alt="Back Arrow" className="absolute left-4 top-3 h-4" />
           </Link>
-          <h2 className="mb-0 ml-18 font-medium text-[20px]">Reset Password</h2>
+          <h2 className="text-[16px] mx-auto">Rest password</h2>
         </div>
 
-        <div className="w-full mt-16">
 
         <form className="">
-          <div className="mb-4">
-            <label htmlFor="new-password" className="block text-gray-700 mb-2 text-[19px]"><strong>New password</strong></label>
-            <input
-              type="password"
-              id="new-password"
-              className="w-full px-4 py-2 border border-gray-300 rounded-lg font-medium focus:outline-none focus:ring focus:ring-blue-500 text-[16px]"
-              placeholder="Enter new password"
-            />
+          <div className="h-[266px] w-[345px] flex flex-col items-center justify-center gap-[24px]">
+
+          <div className="w-[297px] h-[142px] flex flex-col mx-auto gap-[16px]">
+            <div className="w-[297px] h-[63px] flex flex-col gap-[4px]">
+              <label htmlFor="new-password" 
+              className=" text-[#151515] font-medium text-[14px] leading-[100%] tracking-[0]">New Password</label>
+              <input
+                type="password"
+                id="new-password"
+                className="w-[297px] h-[40px] border border-gray-300 rounded-lg 
+            font-medium text-[14px] leading-[100%] tracking-[0] 
+             opacity-100 py-[8px] px-[16px]"
+                placeholder="Enter new password"
+              />
+            </div>
+            <div className="w-[297px] h-[63px] flex flex-col gap-[4px]">
+              <label htmlFor="confirm-password" className="text-[#151515] font-medium text-[14px] leading-[100%] tracking-[0]">Confirm new password</label>
+         <input
+  type="password"
+  id="confirm-password"
+  className="w-[297px] h-[40px] border border-gray-300 rounded-lg 
+            font-medium text-[14px] leading-[100%] tracking-[0] 
+             opacity-100 py-[8px] px-[16px]"
+  placeholder="Confirm new password"
+/>
+
+            </div>
           </div>
-          <div className="mb-6">
-            <label htmlFor="confirm-password" className="block text-gray-700 mb-2 text-[19px]"><strong>Confirm new password</strong></label>
-            <input
-              type="password"
-              id="confirm-password"
-              className="w-full px-4 py-2 border border-gray-300 rounded-lg font-medium focus:outline-none focus:ring focus:ring-blue-500 text-[16px]"
-              placeholder="Confirm new password"
-            />
+
+<button
+  type="submit"
+  className="w-[297px] h-[40px] bg-[#2299fa] text-white text-[16px] font-bold py-2 px-4 rounded-lg"
+>
+  Reset password
+</button>
+
+
           </div>
-          <button
-            type="submit"
-            className="w-full bg-[#2299fa] text-white text-[16px] font-bold py-2 px-4 rounded-lg hover:bg-blue-500 transition duration-300"
-          >
-            Reset Password
-          </button>
         </form>
-        </div>
+
       </div>
     </div>
   );
